e0d327af02221826b3262dc72ae9de402fd02542
Krishna <everyfor0324@gmail.com>
2022-05-20 19:58:20 +0900

    0520 1958

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use App\Enums\UserRole;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       
+        \DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
+        \DB::table('users')->truncate();
+
+        \DB::table('users')->insert([
+            [
+                'id'   => 10,
+                'name' => "Super管理者",
+                'email'=> "admin@gmail.com",
+                'email_verified_at' => now(),
+                'password'=>\Hash::make('qweqweqwe'),
+                'created_at'     => Carbon::now()->subWeek(1),
+                'updated_at'     => Carbon::now()->subWeek(1),
+            ]
+        ]);
+
+        \DB::statement('SET FOREIGN_KEY_CHECKS = 1;');
+    }
+}
